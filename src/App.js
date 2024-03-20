7e01323f679dd6ca30c73621a4477abe2da1a335
kyeongrok <hydena03@gmail.com>
2024-03-20 22:27:41 +0900

    last commit

--- src/App.js
+++ src/App.js
@@ -26,17 +26,55 @@ const choice ={
 }
 function App() {
 const[userSelect, setUserSelect] = useState(null)
-
+const[computerSelect, setComputerSelect] = useState(null)
+const[result,setResult]=useState("")
   const play=(userChoice)=>{
     setUserSelect (choice[userChoice])
+    let computerChoice = randomChoice()
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice))
   };
 
+const judgement = (user,computer)=>{
+
+  // user === computer tie
+  // user == rock, computer == "scissors" user이김
+  // user == rock, computer == paper user짐
+  // user == scissors computer paper user이김
+  // user == paper computer rock user이김
+  //user paper computer siccors user이김
+
+  // if(user.name === computer.name){
+  //   return "tie"
+  // }else if(user.name==="Rock"){
+  //   if(computer === "Scissors"){
+  //     return "Win"
+  //   }else{
+  //     return "lose"
+  //   }
+  //   }
+  // }
+
+  if(user.name === computer.name){
+    return "tie"
+  }else if(user.name==="Rock")return computer.name==="Scissors" ? "win":"lose"
+else if (user.name==="Scissors")return computer.name==="Paper" ? "win":"lose"
+else if(user.name=== "Paper") return computer.name === "Rock" ? "win":"lose"
+};
+
+  const randomChoice=()=>{
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수
+    console.log("item array", itemArray)
+    let randomItem = Math.floor (Math.random() * itemArray.length);
+let final = itemArray[randomItem]
+return choice[final];
+  }
 
   return(
     <div>
     <div className="main">
-     <Box title="You" item={userSelect}/>
-     {/* <Box title="Computer"/> */}
+     <Box title="You" item={userSelect} result={result} />
+     <Box title="Computer" item={computerSelect} result={result}/>
     </div>
     <div className="main">
       <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Box = (props) => {
-    console.log("props",props)
+  const result =
+    props.title === "Computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+      ? props.result === "win"
+        ? "lose"
+        : "win"
+      : props.result;
+
+      return (
+        <div className={`box ${result}`}>
+          <h1>{props.title}</h1>
+          <h2>{props.item && props.item.name}</h2>
+          <img className="item-img" src={props.item && props.item.img} />
+          <h2>{result}</h2>
+        </div>
+      );
+
   return (
-    <div className="box">
-        <h1>{props.title}</h1>
-<img className="item-img" src={props.item && props.item.img} />
-        <h2>WIN</h2>
+    <div className='box'>
+      <h1>{props.title}</h1>
+      <h2>{props.item && props.item.name}</h2>
+      <img className="item-img" src={props.item && props.item.img} />
+      <h2>{result}</h2>
     </div>
   );
 };
+
+
 
 export default Box;
